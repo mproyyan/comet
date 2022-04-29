97b5c3b5fb91daa17451170da33109d529109db5
royyan <mproyyan@gmail.com>
2022-04-29 07:51:09 +0700

    create service container

--- public/index.php
+++ public/index.php
@@ -7,9 +7,6 @@ require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 $app = new Application;
 $router = $app->make(Router::class);
-// echo '<pre>';
-// var_dump($app);
-// die;
 
 $router->get('/', function () {
    echo 'home page';
