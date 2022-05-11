40f452d45ec23839e4edabccc1172f5ecafbc01a
royyan <mproyyan@gmail.com>
2022-05-11 13:46:18 +0700

    make the framework as a template so that it can be reused

--- public/index.php
+++ public/index.php
@@ -10,25 +10,6 @@ require_once dirname(__DIR__) . '/vendor/autoload.php';
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 $app->make(Kernel::class)->bootstrap();
 
-Route::get('/', function () {
-   echo 'home page';
-});
-
-Route::get('/about', function () {
-   echo 'about page';
-});
-
-Route::get('/config', function () {
-   echo '<pre>';
-   var_dump(Config::get('app.providers'));
-});
-
-Route::get('/profile/{username}', function ($username, Kernel $kernel) {
-   echo $username . PHP_EOL;
-   echo '<pre>';
-   print_r($kernel);
-});
-
 Route::get('/test/{foo}', [TestController::class, 'index']);
 
 $app->run();
